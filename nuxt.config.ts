cf799064062c56cecd8af06b99b5b60d2bde0c8d
TIM <tbeckett1211@gmail.com>
2025-07-13 10:58:04 -0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/icon", "@nuxt/ui"],
+  css: ["~/assets/css.css"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="text-4xl text-orange-600">My home page</div>
+  <UButton>Click me to learn more</UButton>
+</template>
